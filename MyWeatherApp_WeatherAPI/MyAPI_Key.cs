b4513d7a792e39050ab2738eb3b53a9cae23a185
Saviska-Asn-It <saviska@asnit.lk>
2024-10-29 09:28:03 +0530

    Api Key Moved

---
+++ MyWeatherApp_WeatherAPI/MyAPI_Key.cs
@@ -0,0 +1,9 @@
+﻿namespace MyWeatherApp_WeatherAPI
+{
+    public static class MyAPI_Key
+    {
+        public static string BaseURL { get; } = "http://api.weatherapi.com/v1";
+
+        public static string Key { get; } = "aa6902796cd14c6990050852240810&q";
+    }
+}
